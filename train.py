5f941c4c4f7f99c036f81245152301d653899b19
uraj29 <56241838+uraj29@users.noreply.github.com>
2024-04-08 02:39:53 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
 import pickle
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -10,7 +10,10 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = GaussianNB().fit(X,y)
+#model = LogisticRegression().fit(X, y) #Score - 0.329
+#model = RandomForestClassifier(n_estimators=100, random_state=42).fit(X, y)  # Score - 0.428
+gbc = GradientBoostingClassifier(n_estimators=100, learning_rate=1, max_depth=25)
+model = gbc.fit(X,y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
